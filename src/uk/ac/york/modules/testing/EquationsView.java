c26d523ad48d253bc83f8f63c808ec600ed988b4
David Morris <davidrowlandmorris@gmail.com>
2015-03-05 01:23:46 +0000

    Improve command-line UI

--- src/uk/ac/york/modules/testing/EquationsView.java
+++ src/uk/ac/york/modules/testing/EquationsView.java
@@ -6,13 +6,19 @@ import javax.swing.JFrame;
 public class EquationsView extends JApplet {
 
 	private static final long serialVersionUID = 1319302888145109147L;
-
+	
 	public static void main(String[] args) {
 		
 		Equation e = null;
 		
-		if (args.length!=1) 
-			return;
+		if (args[0].equals("Help")) {
+			printHelp();
+			System.exit(0);
+		}
+		
+		if (args.length != 1) {
+			complain(args);
+		}
 		
 		if (args[0].equals("FirstOrder")) {
 			e = Equation.createEquationFromType(FirstOrderEquation.class);
@@ -21,13 +27,9 @@ public class EquationsView extends JApplet {
 		} else if(args[0].equals("Fraction")) {
 			e = Equation.createEquationFromType(FractionEquation.class);			
 		} else if(args[0].equals("SecondOrder")) {
-			e = Equation.createEquationFromType(SecondOrderEquation.class);
+			e = Equation.createEquationFromType(SecondOrderEquation.class);			
 		} else {
-			System.out.println("Options are: \n"
-					+ "\tFirstOrder:   f(x) = ax+b\n"
-					+ "\tSecond Order: f(x) = ax^2+bx+c\n"
-					+ "\tSinus:        f(x) = a*(sin(b+x)^c)+d\n"
-					+ "\tFraction:     f(x) = a/(x+b)");
+			complain(args);
 		}
 
 		JFrame f = new JFrame("Equation");
@@ -36,5 +38,28 @@ public class EquationsView extends JApplet {
 		f.add(p);
 		f.setVisible(true);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	private static void complain(String[] args) {
+		StringBuilder b = new StringBuilder();
+		for (int i = 0; i < args.length - 1; i++) {
+			b.append(args[i]);
+			b.append(" ");
+		}
+		if (args.length > 0) {
+			b.append(args[args.length-1]);
+		}
+		final String plural = args.length > 1 ? "s" : "";
+		System.err.println("Unrecognised argument" + plural + " \"" + b.toString() + "\"\n");
+		printHelp();
+		System.exit(0);
+	}
+	
+	private static void printHelp() {
+		System.out.println("Options are: \n"
+				+ "\tFirstOrder:   f(x) = ax+b\n"
+				+ "\tSecond Order: f(x) = ax^2+bx+c\n"
+				+ "\tSinus:        f(x) = a*(sin(b+x)^c)+d\n"
+				+ "\tFraction:     f(x) = a/(x+b)");
 	}
 }
